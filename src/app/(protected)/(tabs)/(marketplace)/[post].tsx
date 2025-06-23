3c154f69cd34142bddd1510082e8d6ac66e3dad0
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-06-23 14:01:41 -0500

    Created empty post details page

---
+++ src/app/(protected)/(tabs)/(marketplace)/[post].tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MarketplaceListingPage = () => {
+  return (
+    <View>
+      <Text>MarketplaceListingPage</Text>
+    </View>
+  )
+}
+
+export default MarketplaceListingPage
+
+const styles = StyleSheet.create({})
