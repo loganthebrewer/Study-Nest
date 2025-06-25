dc80756b4de1300ce475ce8b0b955f10dc46e224
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-06-25 14:47:06 -0500

    Added more fields to new listing page

--- src/app/(protected)/newMarketplacePost.tsx
+++ src/app/(protected)/newMarketplacePost.tsx
@@ -3,22 +3,58 @@ import { Text, View } from '@/components/Themed'
 import Colors from '@/constants/Colors'
 
 import React from 'react'
+import { ScrollView } from 'react-native'
 
 const newMarketplacePost = () => {
   const colorScheme = useColorScheme();
   return (
     <TouchableWithoutFeedback style={styles.container} onPress={() => Keyboard.dismiss()}>
-      <KeyboardAvoidingView behavior='padding' style={[styles.container, {backgroundColor: colorScheme === 'dark' ? Colors.dark.background : Colors.light.background}]} >
-        <Text lightColor={Colors.light.text} darkColor={Colors.dark.text}>Post Title</Text>
+      <ScrollView alwaysBounceHorizontal={false} style={[styles.container, {backgroundColor: colorScheme === 'dark' ? Colors.dark.background : Colors.light.background}, {padding: 10}]}>
         <TextInput
           style={[styles.textInputTitle, {color: colorScheme === 'dark' ? Colors.dark.text : Colors.light.text}]}
-          placeholder="Post Title"
-        />
-        <View style={styles.carousel}>
-          <Image src='https://api.thecatapi.com/v1/images/search'></Image>
+          placeholder="Title"
+          placeholderTextColor={colorScheme === 'dark' ? Colors.dark.textSecondary : Colors.light.textSecondary}
+        /> 
+        <TextInput
+          style={[styles.textInputPrice, {color: colorScheme === 'dark' ? Colors.dark.text : Colors.light.text}]}
+          placeholder="Price"
+          placeholderTextColor={colorScheme === 'dark' ? Colors.dark.textSecondary : Colors.light.textSecondary}
+          caretHidden={true}
+          inputMode='decimal'
+        /> 
+        <View style={[styles.cardContainer, styles.imagesContainer, styles.marginVertical10, { backgroundColor: colorScheme == 'dark' ? Colors.dark.card : Colors.light.card }]}>
+          <Text style={styles.cardTitle}>Images</Text>
+          <ScrollView horizontal={true} style={[styles.carousel]}>
+            <Image
+              source={{ uri: "https://www.wondercide.com/cdn/shop/articles/Upside_down_gray_cat.png?v=1685551065" }}
+              style={styles.carouselImage}
+            />
+            <Image
+              source={{ uri: "https://www.wondercide.com/cdn/shop/articles/Upside_down_gray_cat.png?v=1685551065" }}
+              style={styles.carouselImage}
+            />
+            <Image
+              source={{ uri: "https://www.wondercide.com/cdn/shop/articles/Upside_down_gray_cat.png?v=1685551065" }}
+              style={styles.carouselImage}
+            />
+            <Image
+              source={{ uri: "https://www.wondercide.com/cdn/shop/articles/Upside_down_gray_cat.png?v=1685551065" }}
+              style={styles.carouselImage}
+            />
+            
+          </ScrollView>
         </View>
-        <Text></Text>
-      </KeyboardAvoidingView>
+        
+        
+        <View style={[styles.cardContainer, styles.descriptionContainer, styles.marginVertical10, { backgroundColor: colorScheme === 'dark' ? Colors.dark.card : Colors.light.card }]}>
+          <Text style={styles.cardTitle}>Description</Text>
+          <TextInput
+            style={[styles.textInputDescription, {color: colorScheme === 'dark' ? Colors.dark.text : Colors.light.text},]}
+            placeholder="Description"
+            multiline={true}
+          />
+        </View>
+      </ScrollView>
     </TouchableWithoutFeedback>
   )
 }
@@ -29,21 +65,53 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
   },
+  cardContainer: {
+    borderRadius: 20,
+    padding: 15,
+  },
+  cardTitle: {
+    paddingBottom: 10,
+  },
   textInput: {
-    marginHorizontal: 200
+    marginVertical: 10,
   },  
   textInputTitle: {
     height: 60,
-    borderWidth: 2,
-    fontSize: 50
+    fontSize: 50,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
+  textInputPrice: {
+    height: 50,
+    fontSize: 40,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+  },
+  descriptionContainer: {
+    flex: 1,
+    minHeight: 200,
+  },
+  textInputDescription: {
+    fontSize: 25
+  },
+  imagesContainer: {
+    flex:1,
+    minHeight: 150
   },
   carousel: {
-  
+    
   },
   carouselImage: {
-    aspectRatio: 1
+    height: 100,
+    width: 100,
+    resizeMode: 'cover',
+    borderRadius: 15,
+    marginHorizontal: 5
+  },
+  marginVertical10: {
+    marginVertical: 10,
+  },
+  marginHorizontal10: {
+    marginHorizontal: 10,
   }
-
-
-
 })
