12bffa662c25d86deb3c48ac6963907d430aae49
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-07-07 14:53:53 -0500

    Reworked marketplace card

--- src/components/MarketplacePostCard.tsx
+++ src/components/MarketplacePostCard.tsx
@@ -1,57 +1,68 @@
-import { Pressable, StyleSheet, useColorScheme, } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
-import Colors from '@/constants/Colors'
-import { Image } from 'react-native'
+import { Pressable, StyleSheet } from 'react-native';
+import { Text, View as ThemedView} from '@/components/Themed';
+import { View } from 'react-native';
+import colors from '@/constants/colors';
+import { Image } from 'react-native';
+import { Link, router } from 'expo-router';
+import { useColorScheme } from 'react-native';
+import { LocalMarketplaceListing } from '@/constants/types';
+import { MARKETPLACE_IMAGE_FALLBACK } from '@/constants/fallbacks';
 
-const MarketplacePostCard = () => {
+export const defaultMarketplaceImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ListingListItemProps = {
+    listing: LocalMarketplaceListing;
+};
+
+
+
+const ListingListItem = ({ listing } : ListingListItemProps) => {
     const colorScheme = useColorScheme();
+    const imageBackgroundColor = colorScheme === 'dark' ? colors.dark.card: colors.light.card;
+
+    const containerStyle = StyleSheet.flatten([
+        styles.container,
+        { backgroundColor: colorScheme === 'dark' ? colors.dark.card : colors.light.card }
+    ]); // ran into an odd bug here where if the pressable had any square brackets in the props of the pressable the list would only show 1 column...
 
     return (
-        <Link href="/[post]">
-            <Pressable style={[styles.container, { backgroundColor: colorScheme === 'dark' ? Colors.dark.card : Colors.light.card }]}>
-                <Image
-                    source={{ uri: "https://www.wondercide.com/cdn/shop/articles/Upside_down_gray_cat.png?v=1685551065" }}
-                    style={styles.image}
-                    resizeMode="cover" // or "contain"
-                />
-                <View style={[styles.bottomTextContainer]}>
-                    <Text style={[styles.title, {color: colorScheme === 'dark' ? Colors.dark.text : Colors.light.text}]}>Cat</Text>
-                    <Text style={[styles.price, {color: colorScheme === 'dark' ? Colors.dark.textSecondary : Colors.light.textSecondary}]}>$1</Text>
+        <Link href={{ pathname: '/[id]', params: { id: listing.id.toString() }}} asChild>
+            <Pressable style={containerStyle}>
+                <Image source={{ uri: listing.thumbnail || MARKETPLACE_IMAGE_FALLBACK }} style={[ styles.image, {backgroundColor: imageBackgroundColor}]} />
+                <View style={styles.bottomTextContainer}>
+                    <Text numberOfLines={1} style={ styles.title } lightColor={colors.light.text} darkColor={colors.dark.text}>{ listing.title }</Text>
+                    <Text style={ styles.price } lightColor={colors.light.tint} darkColor={colors.dark.tint}>${ listing.price }</Text>
                 </View>
             </Pressable>
         </Link>
-    )
-}
+  );
+};
 
-export default MarketplacePostCard
+export default ListingListItem;
+
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         padding: 10,
         borderRadius: 15,
-        flex: 1,
-        maxWidth: '10%',
     },
     image: {
         width: '100%',
         aspectRatio: 1,
-        resizeMode: 'cover',
         borderRadius: 15,
     },
+    bottomTextContainer: {
+        flexDirection: 'row',
+        paddingTop: 10,
+        justifyContent: 'space-between'
+    },
     title: {
-        fontSize: 20,
+        fontSize: 15,
         fontWeight: '600',
+        maxWidth: 100
     },
     price: {
-        fontSize: 20,
-        
+        fontSize: 15,
     },
-    bottomTextContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        padding: 10,
-        justifyContent: 'space-between'
-    }
-})
+});
