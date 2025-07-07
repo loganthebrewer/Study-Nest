9f9e668f998ec576769a7fbe25557b3a22451895
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-07-07 13:41:17 -0500

    Created dummy marketplace data

---
+++ assets/data/marketplaceTestData.ts
@@ -0,0 +1,60 @@
+const listings = [
+    {
+        id: 1,
+        title: "Cat 1",
+        thumbnail: "https://cdn2.thecatapi.com/images/d43.jpg",
+        price: 4,
+        status: 1, // 1 = available
+    },
+    {
+        id: 2,
+        title: "Cat 200000000000000000000000000000000",
+        thumbnail: "https://cdn2.thecatapi.com/images/1tm.jpg",
+        price: 4,
+        status: 1, // 1 = available
+    },
+    {
+        id: 3,
+        title: "Cat 3",
+        thumbnail: "https://cdn2.thecatapi.com/images/dh3.jpg",
+        price: 4,
+        status: 1, // 1 = available
+    },
+    {
+        id: 4,
+        title: "Cat 4",
+        thumbnail: "https://cdn2.thecatapi.com/images/71a.jpg",
+        price: 4,
+        status: 1, // 1 = available
+    },
+    {
+        id: 5,
+        title: "Cat 5",
+        thumbnail: "https://cdn2.thecatapi.com/images/9un.jpg",
+        price: 4,
+        status: 1, // 1 = available
+    },
+    {
+        id: 6,
+        title: "Cat 6",
+        thumbnail: "https://cdn2.thecatapi.com/images/7l8.jpg",
+        price: 4,
+        status: 1, // 1 = available
+    },
+    {
+        id: 7,
+        title: "Cat 7",
+        thumbnail: "https://cdn2.thecatapi.com/images/dun.jpg",
+        price: 4,
+        status: 1, // 1 = available
+    },
+    {
+        id: 8,
+        title: "Cat 8",
+        thumbnail: "https://cdn2.thecatapi.com/images/MTkwMjIxNg.gif",
+        price: 4,
+        status: 1, // 1 = available
+    },
+]
+
+export default listings;
