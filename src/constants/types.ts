e8d85a61e6098c1f3c6d00485b2a27edae8ece1a
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-07-07 14:45:27 -0500

    Reworked mp post types to align with supabase

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -1,4 +1,4 @@
-export type MarketplaceListing = {
+export type LocalMarketplaceListing = { //For early testing purposes only. Will be deleted later.
     id: number;
     title: string;
     thumbnail: string | null;
@@ -9,4 +9,21 @@ export type MarketplaceListing = {
 enum MarketplaceListingStatus {
     Sold = 0,
     Available = 1,
+}
+
+export type MarketplaceListing {
+    mp_post_id : string;
+    created_at : Date;
+    updated_at : Date;
+    title : string;
+    description : string;
+    status : LocalMarketplaceListing
+    author_id : string;
+    price : number; 
+}
+
+export type MarketplacePostImage {
+    post_id : string;
+    img_url : string;
+    img_position : number;
 }
