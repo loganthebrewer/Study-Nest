c770d94c2fcb288efbe8c7ccea69d204f6fa00db
loganthebrewer <161977376+loganthebrewer@users.noreply.github.com>
2025-07-08 13:36:04 -0500

    Added ability to remove images when creating marketplace post. Added icon for adding new image. Moved image upload carousel into a component file again.

--- src/app/(protected)/newMarketplacePost.tsx
+++ src/app/(protected)/newMarketplacePost.tsx
@@ -1,52 +1,21 @@
-import { Image, Keyboard, KeyboardAvoidingView, Pressable, StyleSheet, TextInput, TouchableWithoutFeedback, useColorScheme } from 'react-native'
+import { Image, ImageBackground, Keyboard, KeyboardAvoidingView, Pressable, StyleSheet, TextInput, TouchableWithoutFeedback, useColorScheme } from 'react-native'
 import { Text, View } from '@/components/Themed'
 import { View as DefaultView } from 'react-native'
 import colors from '@/constants/colors'
 import { Dimensions } from 'react-native'
 import React, { useState } from 'react'
 import { ScrollView } from 'react-native'
+import * as ImagePicker from 'expo-image-picker';
+import ImageUploadCarousel from '@/components/ImageUploadCarousel'
 
-const ImageUploadCarousel = () => {
-    const colorScheme = useColorScheme();
-
-    const [images, setImages] = useState<string[]>([]);
-    
-    const addItem = () => {
-        setImages((prevItems) => [...prevItems, `${prevItems.length + 1}`]);
-    }
-
-    return (
-    <DefaultView>
-        <ScrollView
-            horizontal
-            style={styles.scrollView}
-            showsHorizontalScrollIndicator={false}
-        >
-        {images.map((item, index) => (
-            <View key={index} style={[styles.box, styles.plusBox, {backgroundColor: colorScheme === 'dark' ? colors.dark.cardSecondary : colors.light.cardSecondary}]}>
-                
-                <Text>{item}</Text>
-            </View>
-        ))}
-        <Pressable onPress={addItem} style={[styles.box, styles.plusBox, {backgroundColor: colorScheme === 'dark' ? colors.dark.cardSecondary : colors.light.cardSecondary}]}>
-            <Text>+</Text>
-        </Pressable>
-        </ScrollView>
-    </DefaultView>
-    )
-}
 
 
 const newMarketplacePost = () => {
   const [postTitle, setTitle] = useState<string>('');
   const [postPrice, setPrice] = useState<number>();
   const [postDescription, setDescription] = useState<string>('');
-  const [postImages, setImages] = useState<Image[]>();
-
-  const [items, setItems] = useState<string[]>([]);
-  const addItem = () => {
-    setItems((prevItems) => [...prevItems, `${prevItems.length + 1}`]);
-  }
+  const [images, setImages] = useState<string[]>([]);
+  
 
   const screenWidth = Dimensions.get('window').width;
   const colorScheme = useColorScheme();
@@ -72,7 +41,7 @@ const newMarketplacePost = () => {
   
         <View style={[styles.cardContainer, styles.imagesContainer, styles.marginVertical10, { backgroundColor: colorScheme == 'dark' ? colors.dark.card : colors.light.card }]}>
           <Text style={styles.cardTitle}>Images</Text>
-          <ImageUploadCarousel></ImageUploadCarousel>
+          <ImageUploadCarousel images={images} setImages={setImages}></ImageUploadCarousel>
         </View>
         
         
@@ -138,29 +107,7 @@ const styles = StyleSheet.create({
     flex:1,
     minHeight: 150
   },
-  carousel: {
-    
-  },
-  scrollView: { },
   scrollContent: { flexDirection: 'row', alignItems: 'center' },
-  box: {
-    width: 100,
-    height: 100,
-    marginHorizontal: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 20,
-  },
-  carouselImage: {
-    height: 100,
-    width: 100,
-    resizeMode: 'cover',
-    borderRadius: 15,
-    marginHorizontal: 5
-  },
-  plusBox: {
-    
-  },
   marginVertical10: {
     marginVertical: 10,
   },
